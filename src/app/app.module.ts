3475f4b8752e257b423dcbfc31bf7d913a155f42
Alex Sorafumo <alex@yuion.net>
2019-04-17 20:33:03 +1000

    Move library files to src root folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { AToggleModule } from '../../lib/src/public_api';
+import { AToggleModule } from '../lib/public_api';
 
 @NgModule({
     declarations: [AppComponent],
